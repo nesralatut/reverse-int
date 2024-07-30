7e9cba64d33a5b394b60dd28b77ef3724b16ccad
Nina Porolonova <127274141+nesralatut@users.noreply.github.com>
2024-07-30 19:49:38 +0300

    fix: change solution

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ module.exports = function reverse(n) {
     const string = n.toString();
     const isNegative = string[0] === '-';    
     const reversedString = isNegative ? string.slice(1).split('').reverse().join('') : string.split('').reverse().join('');    
-    const reversedNumber = parseInt(reversedString);
-    const reverseInt = isNegative ? -reversedNumber : reversedNumber;    
+    const reverseInt = parseInt(reversedString); 
     return reverseInt;
 }
