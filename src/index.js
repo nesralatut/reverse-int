8261213b10bfe5d22564ed4a8ab36ee230aaccf8
Nina Porolonova <127274141+nesralatut@users.noreply.github.com>
2024-07-30 19:41:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const string = n.toString();
+    const isNegative = string[0] === '-';    
+    const reversedString = isNegative ? string.slice(1).split('').reverse().join('') : string.split('').reverse().join('');    
+    const reversedNumber = parseInt(reversedString);
+    const reverseInt = isNegative ? -reversedNumber : reversedNumber;    
+    return reverseInt;
 }
